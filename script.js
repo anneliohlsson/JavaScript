c636ecea2dcf43f4b2b224ee95a060406e28fb6c
Anneli Olsson <anneli.ohlsson@gmail.com>
2023-11-30 16:36:50 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let pizzaPrice = 50;
+let toppingPrize = 10;
+let numberOfToppings = 10;
+let pizzaCost = pizzaPrice + (toppingPrize * numberOfToppings)
+window.alert ('You ordered a pizza with ' + numberOfToppings + ' toppings at $'+ toppingPrize + ' each. The total cost is $' + pizzaCost);
