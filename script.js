151cdd6542a5093fe6b4ed874ce66263e29098ae
Anneli Olsson <anneli.ohlsson@gmail.com>
2023-12-04 16:19:11 +0100

    added new js-file

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-(function () {
-let result, yes = true, no = false
-
-result = yes && yes
-console.log('Are both true?' + result)
-
-result = yes && no
-console.log('Are both still true?' + result)
-
-result = yes || no
-console.log ('Are either true?' + result)
-
-result = no || no
-console.log ('Are either still true?' + result)
-
-console.log ('Original value:' + yes)
-yes = !yes
-console.log('Toggled value' + yes)
-}
-)
-()
+let num = 42;
+const strNum = num.toString();
+{console.log(strNum)}
---
+++ script2.js
@@ -0,0 +1,7 @@
+let num = 42;
+const strNum = num.toString();
+{
+    console.log(strNum)
+    
+}
+{console.log(typeof strNum)}
